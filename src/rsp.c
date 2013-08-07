166b71f21a9f1cbc49b81fd224d55a94f53c10de
Giles Thomas <giles@giles.net>
2013-08-07 13:44:16 +0000

    Removed unwanted binary from repo; added code to accept incoming connections.

--- src/rsp.c
+++ src/rsp.c
@@ -1,12 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main(int arg, char *argv[]) {
+#define MAX_LISTEN_BACKLOG 1
+
+
+void handle_client_connection(int fd) {
+    printf("Handling client connection on %d\n", fd);
+}
+
+
+int main(int argc, char *argv[]) {
     int server_socket_fd;
+    int client_socket_fd;
     struct sockaddr_in server_socket_addr;
+    struct sockaddr_in client_socket_addr;
+    int client_socket_addr_size;
+
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
 
     server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket_fd < 0) {
@@ -23,4 +40,15 @@ int main(int arg, char *argv[]) {
         perror("Could not bind");
         exit(1);
     }
+
+    listen(server_socket_fd, MAX_LISTEN_BACKLOG);
+
+    client_socket_addr_size = sizeof(client_socket_addr);
+    client_socket_fd = accept(server_socket_fd, (struct sockaddr *) &client_socket_addr, &client_socket_addr_size);
+    if (client_socket_fd < 0) {
+        perror("Could not accept");
+        exit(1);
+    }
+
+    handle_client_connection(client_socket_fd);
 }
