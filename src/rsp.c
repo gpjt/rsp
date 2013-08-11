e7171ab4ac6b600f6dadcc46a8569a1e93a42769
Giles Thomas <giles@giles.net>
2013-08-11 21:48:44 +0000

    Rewrote backend connection code to follow man page for getaddrinfo, as apparently gethostbyname is deprecated and we should be using current best practice here.

--- src/rsp.c
+++ src/rsp.c
@@ -10,34 +10,55 @@
 #define BUFFER_SIZE 4096
 
 
-void handle_client_connection(int client_socket_fd, char *backend_addr, int backend_port) {
+void handle_client_connection(int client_socket_fd, 
+                              char *backend_host, 
+                              char *backend_port_str) 
+{
+
+    struct addrinfo hints;
+    struct addrinfo *addrs;
+    struct addrinfo *addrs_iter;
     int backend_socket_fd;
-    struct hostent *backend_server_host;
-    struct sockaddr_in backend_server_addr;
     char buffer[BUFFER_SIZE];
     int bytes_read;
 
-    backend_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (backend_socket_fd < 0) {
-        perror("Couldn't create backend socket");
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;
+
+    if (getaddrinfo(backend_host, backend_port_str, &hints, &addrs) != 0) {
+        perror("Couldn't find backend");
         exit(1);
     }
 
-    backend_server_host = gethostbyname(backend_addr);
-    if (backend_server_host == NULL) {
-        fprintf(stderr, "Could not find host %s\n", backend_addr);
+    for (addrs_iter = addrs; 
+         addrs_iter != NULL; 
+         addrs_iter = addrs_iter->ai_next) 
+    {
+        backend_socket_fd = socket(addrs_iter->ai_family, 
+                                   addrs_iter->ai_socktype,
+                                   addrs_iter->ai_protocol);
+        if (backend_socket_fd == -1) {
+            continue;
+        }
+
+        if (connect(backend_socket_fd, 
+                    addrs_iter->ai_addr, 
+                    addrs_iter->ai_addrlen) != -1) { 
+            break;
+        }
+
+        close(backend_socket_fd);
+    }
+
+    if (addrs_iter == NULL) {
+        fprintf(stderr, "Couldn't connect to backend");
         exit(1);
     }
 
-    memset(&backend_server_addr, 0, sizeof(backend_server_addr));
-    backend_server_addr.sin_family = AF_INET;
-    memcpy(&backend_server_addr.sin_addr.s_addr, backend_server_host->h_addr, backend_server_host->h_length);
-    backend_server_addr.sin_port = htons(backend_port);
-
-    if (connect(backend_socket_fd, (struct sockaddr *) &backend_server_addr, sizeof(backend_server_addr)) < 0) {
-        perror("Could not connect to backend");
-        exit(1);
-    }
+    freeaddrinfo(addrs);
 
     bytes_read = read(client_socket_fd, buffer, BUFFER_SIZE);
     write(backend_socket_fd, buffer, bytes_read);
@@ -56,7 +77,7 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in client_socket_addr;
     int client_socket_addr_size;
     char *backend_addr;
-    int backend_port;
+    char *backend_port_str;
 
     if (argc != 4) {
         printf("Usage: %s <server_port> <backend_addr> <backend_port>\n", argv[0]);
@@ -64,7 +85,7 @@ int main(int argc, char *argv[]) {
     }
     server_port = atoi(argv[1]);
     backend_addr = argv[2];
-    backend_port = atoi(argv[3]);
+    backend_port_str = argv[3];
 
     server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket_fd < 0) {
@@ -91,5 +112,5 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    handle_client_connection(client_socket_fd, backend_addr, backend_port);
+    handle_client_connection(client_socket_fd, backend_addr, backend_port_str);
 }
