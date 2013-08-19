db83ee89062cf009bd1e2d81b6f230334c8fbca8
Giles Thomas <giles@giles.net>
2013-08-19 19:26:59 +0000

    Moved stuff to create and start server socket to server_socket.c

--- src/rsp.c
+++ src/rsp.c
@@ -15,7 +15,6 @@
 #include "server_socket.h"
 
 
-#define MAX_LISTEN_BACKLOG 1
 #define BUFFER_SIZE 4096
 
 
@@ -184,67 +183,12 @@ void handle_client_connection(int epoll_fd,
 }
 
 
-int create_and_bind(char *server_port_str) {
-    int server_socket_fd;
-
-    struct addrinfo hints;
-    struct addrinfo *addrs;
-    struct addrinfo *addr_iter;
-    int getaddrinfo_error;
-
-    int so_reuseaddr;
-
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    getaddrinfo_error = getaddrinfo(NULL, server_port_str, &hints, &addrs);
-    if (getaddrinfo_error != 0) {
-        fprintf(stderr, "Couldn't find local host details: %s\n", gai_strerror(getaddrinfo_error));
-        exit(1);
-    }
-
-    for (addr_iter = addrs; addr_iter != NULL; addr_iter = addr_iter->ai_next) {
-        server_socket_fd = socket(addr_iter->ai_family,
-                                  addr_iter->ai_socktype,
-                                  addr_iter->ai_protocol);
-        if (server_socket_fd == -1) {
-            continue;
-        }
-
-        so_reuseaddr = 1;
-        setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &so_reuseaddr, sizeof(so_reuseaddr));
-
-        if (bind(server_socket_fd, 
-                 addr_iter->ai_addr, 
-                 addr_iter->ai_addrlen) == 0) 
-        {
-            break;
-        }
-
-        close(server_socket_fd);
-    }
-
-    if (addr_iter == NULL) {
-        fprintf(stderr, "Couldn't bind\n");
-        exit(1);
-    }
-
-    freeaddrinfo(addrs);
-
-    return server_socket_fd;
-}
-
-
 int main(int argc, char *argv[]) {
     char *server_port_str;
     char *backend_addr;
     char *backend_port_str;
 
     int epoll_fd;
-
-    int server_socket_fd;
     struct epoll_event_handler_data *server_socket_event_handler;
 
 
@@ -258,18 +202,16 @@ int main(int argc, char *argv[]) {
     backend_addr = argv[2];
     backend_port_str = argv[3];
 
-    server_socket_fd = create_and_bind(server_port_str);
-    make_socket_non_blocking(server_socket_fd);
-
-    listen(server_socket_fd, MAX_LISTEN_BACKLOG);
-
     epoll_fd = epoll_create1(0);
     if (epoll_fd == -1) {
         perror("Couldn't create epoll FD");
         exit(1);
     }
 
-    server_socket_event_handler = create_server_socket_handler(server_socket_fd, epoll_fd, backend_addr, backend_port_str);
+    server_socket_event_handler = create_server_socket_handler(epoll_fd, 
+                                                               server_port_str,
+                                                               backend_addr,
+                                                               backend_port_str);
 
     add_epoll_handler(epoll_fd, server_socket_event_handler, EPOLLIN);
 
--- src/server_socket.c
+++ src/server_socket.c
@@ -1,9 +1,16 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 #include "epollinterface.h"
 #include "server_socket.h"
+
+#define MAX_LISTEN_BACKLOG 10
 
 
 int handle_server_socket_event(struct epoll_event_handler_data *self, uint32_t events) {
@@ -30,9 +37,69 @@ int handle_server_socket_event(struct epoll_event_handler_data *self, uint32_t e
 }
 
 
-struct epoll_event_handler_data *create_server_socket_handler(int server_socket_fd, int epoll_fd, char *backend_addr, char *backend_port_str) {
+int create_and_bind(char *server_port_str) {
+    int server_socket_fd;
+
+    struct addrinfo hints;
+    struct addrinfo *addrs;
+    struct addrinfo *addr_iter;
+    int getaddrinfo_error;
+
+    int so_reuseaddr;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo_error = getaddrinfo(NULL, server_port_str, &hints, &addrs);
+    if (getaddrinfo_error != 0) {
+        fprintf(stderr, "Couldn't find local host details: %s\n", gai_strerror(getaddrinfo_error));
+        exit(1);
+    }
+
+    for (addr_iter = addrs; addr_iter != NULL; addr_iter = addr_iter->ai_next) {
+        server_socket_fd = socket(addr_iter->ai_family,
+                                  addr_iter->ai_socktype,
+                                  addr_iter->ai_protocol);
+        if (server_socket_fd == -1) {
+            continue;
+        }
+
+        so_reuseaddr = 1;
+        setsockopt(server_socket_fd, SOL_SOCKET, SO_REUSEADDR, &so_reuseaddr, sizeof(so_reuseaddr));
+
+        if (bind(server_socket_fd,
+                 addr_iter->ai_addr,
+                 addr_iter->ai_addrlen) == 0)
+        {
+            break;
+        }
+
+        close(server_socket_fd);
+    }
+
+    if (addr_iter == NULL) {
+        fprintf(stderr, "Couldn't bind\n");
+        exit(1);
+    }
+
+    freeaddrinfo(addrs);
+
+    return server_socket_fd;
+}
+
+
+struct epoll_event_handler_data *create_server_socket_handler(int epoll_fd, char *server_port_str, char *backend_addr, char *backend_port_str) {
     struct epoll_event_handler_data *result;
     struct server_socket_event_data *closure;
+
+    int server_socket_fd;
+
+    server_socket_fd = create_and_bind(server_port_str);
+    make_socket_non_blocking(server_socket_fd);
+
+    listen(server_socket_fd, MAX_LISTEN_BACKLOG);
 
     closure = malloc(sizeof(struct server_socket_event_data));
     closure->epoll_fd = epoll_fd;
