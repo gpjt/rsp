9f4559c8bd95d7d22403c6fbb6c5f8f74b1eb7f2
Giles Thomas <giles@giles.net>
2013-08-19 19:56:20 +0000

    Moved client socket handling stuff to separate source files

---
+++ src/client_socket.c
@@ -0,0 +1,44 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/epoll.h>
+
+#include "epollinterface.h"
+#include "client_socket.h"
+
+#define BUFFER_SIZE 4096
+
+
+int handle_client_socket_event(struct epoll_event_handler_data *self, uint32_t events) {
+    struct client_socket_event_data *closure;
+    char buffer[BUFFER_SIZE];
+    int bytes_read;
+
+    closure = (struct client_socket_event_data *) self->closure;
+
+    if ((events & EPOLLERR) | (events & EPOLLHUP) | (events & EPOLLRDHUP)) {
+        close(self->fd);
+        close(closure->backend_socket_fd);
+        free(closure);
+        return 1;
+    }
+
+    if (events & EPOLLIN) {
+        bytes_read = read(self->fd, buffer, BUFFER_SIZE);
+        if (bytes_read == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            return 0;
+        }
+
+        if (bytes_read == 0 || bytes_read == -1) {
+            close(self->fd);
+            close(closure->backend_socket_fd);
+            free(closure);
+            return 1;
+        }
+
+        write(closure->backend_socket_fd, buffer, bytes_read);
+    }
+
+    return 0;
+}
+
---
+++ src/client_socket.h
@@ -0,0 +1,7 @@
+struct client_socket_event_data {
+    int backend_socket_fd;
+};
+
+
+
+extern int handle_client_socket_event(struct epoll_event_handler_data *self, uint32_t events);
