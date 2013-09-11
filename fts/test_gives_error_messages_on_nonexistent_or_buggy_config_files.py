ee95abeead695d75681e60e1704f4835ac315780
Giles Thomas <giles@giles.net>
2013-09-11 13:34:11 +0000

    Wrote FTs for basic config file parsing

--- fts/test_gives_error_messages_on_nonexistent_or_buggy_config_files.py
+++ fts/test_gives_error_messages_on_nonexistent_or_buggy_config_files.py
@@ -15,9 +15,14 @@ RSP_BINARY = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "rsp"
 class TestGivesAppropriateErrorMessagesOnNonExistentOrBuggyConfigFiles(unittest.TestCase):
 
     def test_gives_appropriate_error_message_on_nonexistent_config_file(self):
-        self.fail("TODO")
+        server = pexpect.spawn(RSP_BINARY, ["/definitely_does_not_exist"])
+        server.expect("Error parsing config file: cannot open /definitely_does_not_exist: No such file or directory")
 
 
     def test_gives_appropriate_error_message_on_buggy_config_file(self):
-        self.fail("TODO")
+        _, config_file = mkstemp(suffix=".lua")
+        with open(config_file, "w") as f:
+            f.write("dfs'df';f'#as'\n")
+        server = pexpect.spawn(RSP_BINARY, [config_file])
+        server.expect("Error parsing config file:")
 
