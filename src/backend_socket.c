6fad3ae20136efb3fbb7497f750dc274b9df8152
Giles Thomas <giles@giles.net>
2013-10-18 13:52:27 +0000

    Made creating the backend socket handler look pretty much like creating the client socket handler

--- src/backend_socket.c
+++ src/backend_socket.c
@@ -14,11 +14,6 @@
 #define BUFFER_SIZE 4096
 
 
-struct backend_socket_event_data {
-    struct epoll_event_handler* client_handler;
-};
-
-
 void handle_backend_socket_event(struct epoll_event_handler* self, uint32_t events)
 {
     struct backend_socket_event_data* closure = (struct backend_socket_event_data*) self->closure;
@@ -62,13 +57,11 @@ void close_backend_socket(struct epoll_event_handler* self)
 
 
 struct epoll_event_handler* create_backend_socket_handler(int epoll_fd, 
-                                                          int backend_socket_fd, 
-                                                          struct epoll_event_handler* client_handler)
+                                                          int backend_socket_fd) 
 {
     make_socket_non_blocking(backend_socket_fd);
 
     struct backend_socket_event_data* closure = malloc(sizeof(struct backend_socket_event_data));
-    closure->client_handler = client_handler;
 
     struct epoll_event_handler* result = malloc(sizeof(struct epoll_event_handler));
     result->fd = backend_socket_fd;
--- src/backend_socket.h
+++ src/backend_socket.h
@@ -1,5 +1,9 @@
+struct backend_socket_event_data {
+    struct epoll_event_handler* client_handler;
+};
+
 extern void handle_backend_socket_event(struct epoll_event_handler* self, uint32_t events);
 
 extern void close_backend_socket(struct epoll_event_handler*);
 
-extern struct epoll_event_handler* create_backend_socket_handler(int epoll_fd, int backend_socket_fd, struct epoll_event_handler* client_handler);
+extern struct epoll_event_handler* create_backend_socket_handler(int epoll_fd, int backend_socket_fd);
