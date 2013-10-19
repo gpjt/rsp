8148fc7de8f1cae3d8a2860ef70e169da4e58671
Giles Thomas <giles@giles.net>
2013-10-19 19:39:11 +0000

    Removed unnecessary export from connection

--- src/connection.h
+++ src/connection.h
@@ -8,8 +8,6 @@ struct connection_closure {
     struct data_buffer_entry* write_buffer;
 };
 
-extern void connection_handle_event(struct epoll_event_handler* self, uint32_t events);
-
 extern void connection_write(struct epoll_event_handler* self, char* data, int len);
 
 extern void connection_close(struct epoll_event_handler* self);
