1b90d5c60f95547d2ab18e60075b8b6b33970c4f
Giles Thomas <giles@giles.net>
2013-10-22 19:16:23 +0000

    Added timestamps to logs

--- src/logging.c
+++ src/logging.c
@@ -1,10 +1,24 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <time.h>
+#include <sys/time.h>
 
 
 void rsp_log(char* format, ...)
 {
+    char without_ms[64];
+    char with_ms[64];
+    struct timeval tv;
+    struct tm *tm;
+
+    gettimeofday(&tv, NULL);
+    if ((tm = localtime(&tv.tv_sec)) != NULL)
+    {
+        strftime(without_ms, sizeof(without_ms), "%Y-%m-%d %H:%M:%S.%%06u %z", tm);
+        snprintf(with_ms, sizeof(with_ms), without_ms, tv.tv_usec);
+        fprintf(stdout, "[%s] ", with_ms); 
+    }
+
     va_list argptr;
     va_start(argptr, format);
     vfprintf(stdout, format, argptr);
